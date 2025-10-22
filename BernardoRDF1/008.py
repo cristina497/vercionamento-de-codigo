dc3628296ee45af5c50670decf76c3b260fcd253
henriiiq46 <bernardohmg35@gmail.com>
2025-10-22 11:13:36 -0300

    Create 008.py

---
+++ BernardoRDF1/008.py
@@ -0,0 +1,8 @@
+LIMIT = 5
+frutas = []
+for i in range(LIMIT):
+    nomes = input(f"Digite o nome das {i+1}ª fruta:")
+    print(f"nomes digitados {i+1}º")
+
+print("lista atualizada de frutas:",frutas ) 
+
